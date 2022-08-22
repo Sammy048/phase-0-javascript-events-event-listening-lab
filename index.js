02061f4e1f910647e094f7557d046fa133557551
Sammy048 <sammymasib000@gmail.com>
2022-08-22 10:57:21 +0300

    Updated the assignment

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function addingEventListener() {
-    document.getElementById("button").addEventListener('click', ()=>{
-        alert("I am clicked")
-    })
+// function addingEventListener() {
+//     document.getElementById("button").addEventListener('click', ()=>{
+//         alert("I am clicked")
+//     })
+// }
+// addingEventListener();
+function clickAlert(){
+    return alert('I was clicked!');
+ }
+
+function addingEventListener(){
+    const input = document.getElementById('button');
+    
+    input.addEventListener('click', clickAlert);
 }
-addingEventListener();
